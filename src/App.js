7d0789b82e40b09c2554937d0910a509c4781123
yelena guliyeva <gulbadam@gulbadam.com>
2018-07-27 13:30:45 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-//import React, { Component } from 'react';
+
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-
-//import logo from './logo.svg';
 import './App.css';
-//import Card from "./components/Card";
-//import {robots} from './robots';
 import SearchBox from './components/SearchBox'
 import Scroll from "./components/Scroll"
 
 import Cardlist from "./components/Cardlist";
-// const state ={
-//   robots: robots,
-//   searchfiels: ""
-// }
 import './App.css';
 
 import {setSearchField, requestRobots} from './actions';
@@ -32,64 +24,24 @@ const mapDispatchToProps = (dispatch) => {
     onRequestRobots: () => requestRobots(dispatch)
   };
 };
-
- class App extends Component {
-  //  constructor() {
-  //    super()
-  //    this.state = {
-  //      robots: []
-  //      //searchfield: ""
-  //    }
-  //  }
-   componentDidMount(){
-     this.props.onRequestRobots();
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    // .then(response =>  response.json())
-    //  .then(users => this.setState({robots: users})
-    //  )
-    
-   }
-  //  onSearchChange=(event)=> {
-  //    this.setState({searchfield: event.target.value})
-     
-  //  }
-   render(){ 
-     //const {robots} =this.state;
-     const {searchField, onSearchChange, robots, isPending} = this.props;
-     const filteredRobots = robots.filter(robots => {
-       return robots.name.toLowerCase().includes(searchField.toLowerCase())
-     })
-     //console.log(filterRobots);
-     return isPending?
-     //if (robots.lenght ===0) {
-       <h1>loading</h1> :
-     (
-  <div className="tc">
-         <h1 className=" self-center f1  pa3 mh5 shadow-3 hover-bg-gold">Cats</h1>
+class App extends Component {
+  componentDidMount(){
+    this.props.onRequestRobots();
+}
+  render(){ 
+    const {searchField, onSearchChange, robots, isPending} = this.props;
+    const filteredRobots = robots.filter(robots => {
+      return robots.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+    return isPending?
+      <h1>loading</h1> :
+    (<div className="tc">
+      <h1 className=" self-center f1  pa3 mh5 shadow-3 hover-bg-gold">Cats</h1>
   <SearchBox searchChange={onSearchChange} />
   <Scroll>
   <Cardlist robots={filteredRobots}/>
-     </Scroll>
-
-    </div>
-     )
-    
+  </Scroll>
+</div>)
 }
- }
-//class App extends Component {
- // render() {
-    //return (
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h1 className="App-title">Welcome to React</h1>
-      //   </header>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
-    //);
-  //}
-//}
-
+}
 export default connect(mapStateToProps, mapDispatchToProps) (App);
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/Card.css"
 const Card= ({name, email, id}) => (
-    <div className="tc bg-light-green grow dib f5-ns  black-95 pa3 ph2 ma2 ba b--light-silver shadow-3 br2 pointer">
+    <div className="tc bg-light-green grow dib mw5 f5-ns  black-95 pa3 ph2 ma2 ba b--light-silver shadow-3 br2 pointer">
         <img alt="" src={`https://robohash.org/${id}.png?set=set4`} />
     <div>
     <h2>{name}</h2>

a205aa471af2f11fad5a94d7e3058787cccae7d2
yelena guliyeva <gulbadam@gulbadam.com>
2018-08-08 16:44:25 -0700

    reducer test

--- src/components/Cardlist.test.js
+++ src/components/Cardlist.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Cardlist from './Cardlist';
-it('expect to render Card Component', () => {
-    
-    expect(shallow( <Cardlist /> ).length).toEqual(1)
-});
 it('expect to render Card snp', () => {
     const mockRobots = [{
         id: 1,
